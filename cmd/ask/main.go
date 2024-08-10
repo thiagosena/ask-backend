1aeb4a43ac5ea195a5bc767ce539c721d8266ac3
Thiago Sena <thiagosena.dev@gmail.com>
2024-08-09 22:19:31 -0300

    feat: create structure

---
+++ cmd/ask/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/thiagosena/ask-backend/internal/api"
+	"github.com/thiagosena/ask-backend/internal/store/pgstore"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s",
+		os.Getenv("ASK_DATABASE_HOST"),
+		os.Getenv("ASK_DATABASE_PORT"),
+		os.Getenv("ASK_DATABASE_USER"),
+		os.Getenv("ASK_DATABASE_PASSWORD"),
+		os.Getenv("ASK_DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":"+os.Getenv("PORT"), handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}
